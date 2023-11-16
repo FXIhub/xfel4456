2e3632de26f77eeacf4f235babbe464edb3aa5ea
Kartik Ayyer <kartik.ayyer@desy.de>
2023-11-16 17:04:14 +0100

    constants: Analysis parameters for beamtime

---
+++ offline/constants.py
@@ -0,0 +1,10 @@
+'''Beamtime processing constants for p4456'''
+
+PREFIX = '/gpfs/exfel/exp/SPB/202302/p004456/scratch/'
+FRAME_SHAPE = (8,512,1024)
+VDS_DATASET = '/entry_1/instrument_1/detector_1/data'
+VDS_MASK_DATASET = '/entry_1/instrument_1/detector_1/mask'
+ADU_PER_PHOTON = 17.0
+
+STREAK_PTHRESHOLD = 99.5
+STREAK_MIN_PIX = 15
