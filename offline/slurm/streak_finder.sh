59ee9f5efa46e730c621306bd5ba20b96577cd25
Kartik Ayyer <kartik.ayyer@desy.de>
2023-11-17 11:10:45 +0100

    streak_finder.sh: Fixed call. Adding XGM values

--- offline/slurm/streak_finder.sh
+++ offline/slurm/streak_finder.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-#SBATCH --array=40
+#SBATCH --array=
 #SBATCH --time=04:00:00
 #SBATCH --partition=upex
-##SBATCH --reservation=upex_004456
 #SBATCH --export=ALL
 #SBATCH -J streak_finder
-#SBATCH -o .%A_%a.out
-#SBATCH -e .%A_%a.out
+#SBATCH -o .streak-%4a-%j.out
+#SBATCH -e .streak-%4a-%j.out
 
-# Change the runs to process using the --array option on line 3
+####SBATCH --partition=upex-beamtime
+####SBATCH --reservation=upex_004456
 
 # Load modules and environment
 source /etc/profile.d/modules.sh
 module load exfel exfel-python
 
-python ../streak_finder.py ${SLURM_ARRAY_TASK_ID} --ADU_per_photon 8 --percentile_threshold 99.5 --min_pix 15
-
-
+python ../streak_finder.py ${SLURM_ARRAY_TASK_ID}
+python ../add_pulsedata.py ${SLURM_ARRAY_TASK_ID}
 
