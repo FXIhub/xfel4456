92f90c3bee64b5cfd7d02dc863c307bf60cd4f66
Kartik Ayyer <kartik.ayyer@desy.de>
2023-11-17 10:33:32 +0100

    constants: Added XGM information

--- offline/constants.py
+++ offline/constants.py
@@ -1,6 +1,7 @@
 '''Beamtime processing constants for p4456'''
 
 PREFIX = '/gpfs/exfel/exp/SPB/202302/p004456/scratch/'
+DET_NAME = 'SPB_IRDA_JF4M'
 FRAME_SHAPE = (8,512,1024)
 VDS_DATASET = '/entry_1/instrument_1/detector_1/data'
 VDS_MASK_DATASET = '/entry_1/instrument_1/detector_1/mask'
@@ -8,3 +9,6 @@ ADU_PER_PHOTON = 17.0
 
 STREAK_PTHRESHOLD = 99.5
 STREAK_MIN_PIX = 15
+
+XGM_DA_NUM = 2
+XGM_DATASET = "INSTRUMENT/SPB_XTD9_XGM/XGM/DOOCS:output/data/intensitySa1TD"
