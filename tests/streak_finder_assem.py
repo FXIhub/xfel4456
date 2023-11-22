4b5e22155de00bed92adc42fd85bf52510591865
Chufeng Li <chufeng.li@cfel.de>
2023-11-22 14:19:31 +0100

    add bkg file to streak finder assem

--- tests/streak_finder_assem.py
+++ tests/streak_finder_assem.py
@@ -4,9 +4,9 @@ streak finding for the assembled image from JUNGFRAU detector.
 
 import argparse
 PREFIX      = '/gpfs/exfel/exp/SPB/202302/p004456/'
-geom_file = '/gpfs/exfel/exp/XMPL/201750/p700000/proc/r0040/j4m-p2805_v03.geom'
-
-proposal = 700000
+# geom_file = '/gpfs/exfel/exp/XMPL/201750/p700000/proc/r0040/j4m-p2805_v03.geom'
+geom_file = '/gpfs/exfel/exp/SPB/202302/p004456/usr/geometry/geom_v5.geom'
+proposal = 4456
 parser = argparse.ArgumentParser(description='Find streaks in assembled image and output list to scratch/peak_info/run<run>.h5.')
 parser.add_argument('--run', type=int, help='Run number', required=True)
 parser.add_argument('--thld', type= int, help='litpixel threshold', default=10)
@@ -14,6 +14,7 @@ parser.add_argument('--min_pix', type=int,help='minimum connected pixels for a s
 parser.add_argument('--max_pix', type=int, help='Maximum pixels', default=10000)
 parser.add_argument('--min_peak', type=int, help='Minimum peak',default=5 )
 parser.add_argument('--mask_file', type=str, help='Mask file path', default='None')
+parser.add_argument('--bkg_file', type=str, help='background file path', default='None')
 parser.add_argument('--region', type=str, help='Region: "ALL/Q/C" ', default='ALL')
 
 args = parser.parse_args()
@@ -48,8 +49,8 @@ def worker(rank,no_trains):
     output_log_file = PREFIX + f'/scratch/usr/Shared/butolama/run_{args.run:d}_log_worker{rank:d}.txt'
     output_pickle_file = PREFIX + f'/scratch/usr/Shared/butolama/run_{args.run:d}_peak_info_worker{rank:d}.pkl'
     output_evt_lst_file = PREFIX + f'/scratch/usr/Shared/butolama/run_{args.run:d}_evt_lst_worker{rank:d}.txt'
-       
-        
+
+
     for file_path in [output_log_file, output_pickle_file, output_evt_lst_file]:
         if os.path.exists(file_path):
             print(f'Deleting existing file: {file_path}')
@@ -67,11 +68,12 @@ def worker(rank,no_trains):
         max_pix=args.max_pix,
         min_peak=args.min_peak,
         mask_file=args.mask_file,
+        bgk_file = args.bkg_file,
         region=args.region)
-    
-    
 
-def List_streak_finder(rank,frame_id_lst,output_log_file,output_pickle_file,output_evt_lst_file,thld,min_pix,max_pix,min_peak,mask_file,region):
+
+
+def List_streak_finder(rank,frame_id_lst,output_log_file,output_pickle_file,output_evt_lst_file,thld,min_pix,max_pix,min_peak,mask_file,bkg_file,region):
 
     if len(frame_id_lst)==0:
         sys.exit(f'No frames found')
@@ -98,13 +100,33 @@ def List_streak_finder(rank,frame_id_lst,output_log_file,output_pickle_file,outp
     if mask_file!='None':
         mask_file=os.path.abspath(args.mask_file)
         m=h5py.File(mask_file,'r')
-        mask=m['/data/data'][x_min:x_max,y_min:y_max].astype(bool)
+        mask=m['/entry_1/goodpixels'][x_min:x_max,y_min:y_max].astype(bool)
         m.close()
+        geom = JUNGFRAUGeometry.from_crystfel_geom(geom_file)
+        mask, center = geom.position_modules_fast(mask) ### to assmeble the mask from geometry
     elif mask_file=='None':
         mask=np.ones((IMG_SHAPE[0],IMG_SHAPE[1])).astype(bool)
         mask=mask[x_min:x_max,y_min:y_max]
     else:
         sys.exit('the mask file option is improper.')
+
+    ############## adding a background from a file.
+    if bkg_file!='None':
+        bkg_file=os.path.abspath(args.bkg_file)
+        b=h5py.File(bkg_file,'r')
+        bkg=b['/entry_1/data/white_field'][x_min:x_max,y_min:y_max]
+        b.close()
+        geom = JUNGFRAUGeometry.from_crystfel_geom(geom_file)
+        bkg, center = geom.position_modules_fast(bkg)
+    elif bkg_file=='None':
+        bkg = 0
+    else:
+        sys.exit('the mask file option is improper.')
+
+
+    ##############
+
+
     HIT_counter=0
     HIT_frame_id_list=[]
     peakXPosRaw=np.zeros((len(frame_id_lst),1024))
@@ -118,13 +140,12 @@ def List_streak_finder(rank,frame_id_lst,output_log_file,output_pickle_file,outp
 
     lf.write(f'run : {args.run:d}, worker_rank: {rank:}')
     lf.write('\n-----------')
-    lf.write('\nthld: %d\nmin_pix: %d\nmax_pix: %d\nmin_peak: %d\nmask_file: %s\nRegion: %s'%\
-    (thld,min_pix,max_pix,min_peak,mask_file,region))
+    lf.write('\nthld: %d\nmin_pix: %d\nmax_pix: %d\nmin_peak: %d\nmask_file: %s\nbkg_file: %s\nregion: %s'%\
+    (thld,min_pix,max_pix,min_peak,mask_file,bkg_file,region))
     lf.write('\n-----------')
 
     for event_no in range(len(frame_id_lst)):
         frame_id = frame_id_lst[event_no]
-        bkg = 0
 
         train_img_dict = CBD_ut.read_train(proposal,args.run,frame_id,\
         geom_file=geom_file,geom_assem='True',ROI=(0,2400,0,2400))
