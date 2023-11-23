b4acfb3f06083e7e72971e43939e353a973989a2
Chufeng Li <chufeng.li@cfel.de>
2023-11-23 16:28:48 +0100

    bug fixed

--- offline/streak_finder_assem.py
+++ offline/streak_finder_assem.py
@@ -157,7 +157,7 @@ def List_streak_finder(rank,frame_id_lst,output_log_file,output_pickle_file,outp
 
         img_array = img_array*mask
         bkg = bkg*mask
-        scale_factor = np.nansum(img_arry)/np.nansum(bkg)
+        scale_factor = np.nansum(img_array)/np.nansum(bkg)
 
         img_array_bgd_subtracted = img_array - scale_factor*bkg
         bimg_masked = (img_array_bgd_subtracted > thld) * mask
