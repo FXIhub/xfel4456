25555f5d8b0e5f29e7ae8e17d8e990f797954b77
Ivan De Gennaro Aquino <degennaroaquino.ivan@outlook.com>
2023-11-23 14:15:51 +0100

    Update conf.py

--- online/hummingbird/conf.py
+++ online/hummingbird/conf.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 import numpy as np
 import h5py
 import extra_geom
@@ -26,6 +27,7 @@ state["EuXFEL/SelModule"] = None
 state["EuXFEL/MaxTrainAge"] = 4e20
 state["EuXFEL/FirstCell"] = 0
 state["EuXFEL/LastCell"] = 15
+state["running_wfs"] = None
 
 # jf1m_offsets = [(95.0, 564.0), (95.0, 17.0)]
 # jf1m_orient = [(-1, -1), (-1, -1)]
@@ -47,9 +49,9 @@ wavelength = 10.33e-10
 # streak finder parameters ---------------------------------------------
 
 streak_finder_thr_percent = 99.5
-streak_finder_min_pixels = 10
-streak_finder_max_area = 2000
-streak_finder_num_streaks_threshold = 10
+streak_finder_min_pixels = 20
+streak_finder_max_area = 800
+streak_finder_num_streaks_threshold = 5
 
 # masks ---------------------------------------------------------------
 
@@ -61,26 +63,38 @@ user_mask = np.ones(geom.expected_data_shape, bool)
 # user_mask[:, :, :mrg] = False
 # user_mask[:, :, -mrg:] = False
 
-mask_file = f"{prop_dir}/usr/Shared/hummingbird/mask_for_hummingbird.h5"
+mask_file = f"{prop_dir}/usr/Shared/hummingbird/hummingbird_current_mask.h5"
 with h5py.File(mask_file, "r") as maskh5:
     mask_h5 = np.array(maskh5["entry_1/goodpixels"])
 
+ring_mask_file = f"{prop_dir}/usr/Shared/hummingbird/mask_combined_with_ring_r0085.h5"
+with h5py.File(ring_mask_file, "r") as maskh5:
+    ring_mask_h5 = np.array(maskh5["entry_1/goodpixels"])
+
+mask_h5 = ring_mask_h5 & mask_h5
+
 # background whitefield file (dividing) ------------------------------------------
 
-whitefield_file = f"{prop_dir}/usr/Shared/hummingbird/white_field_for_hummingbird.h5"
+whitefield_file = f"{prop_dir}/usr/Shared/hummingbird/white_field_zeros.h5"
 with h5py.File(whitefield_file, "r") as bgh5:
     background_data = np.array(bgh5["entry_1/data/white_field"])
 
+# running white field background ------------------------------------------------
 
-# operations ----------------------------------------------------------------
+running_wfs = [np.ones(geom.expected_data_shape, float)]
+length_running_white_fields = 1000
+
+# operations ------------------------------------------------------------------
 
 send_hits = True
 send_powdersum = True
 send_profiles = False
 do_peakfinding = False
 do_streakfinding = True
-apply_background_division = True
-apply_background_subtraction = not apply_background_division
+
+apply_running_background_division = False
+apply_background_division = False
+apply_background_subtraction = True  # not apply_background_division
 
 
 if do_streakfinding:
@@ -148,15 +162,25 @@ def onEvent(evt):
 
     det_data = np.moveaxis(det.data[:, 0], (1, 2), (2, 1))
 
-    if apply_background_division and not apply_background_subtraction:
-        background_data[np.isnan(background_data)] = 1
-        background_data[background_data <= 1] = 1
-        det_data = det_data / background_data
+    if apply_running_background_division:
+        if len(running_wfs) > length_running_white_fields:
+            running_wfs.pop(0)
+        running_wfs.append(det_data)
 
-    if apply_background_subtraction and not apply_background_division:
-        background_data[np.isnan(background_data)] = 0
-        background_data[background_data <= 1] = 0
-        det_data = det_data - background_data
+        running_white_field = np.mean(running_wfs, axis=0, dtype=float)
+        running_white_field[np.isnan(running_white_field)] = 1
+        running_white_field[background_data <= 0] = 1
+        det_data = det_data / running_white_field
+    else:
+        if apply_background_division and not apply_background_subtraction:
+            background_data[np.isnan(background_data)] = 1
+            background_data[background_data <= 1] = 1
+            det_data = det_data / background_data
+
+        if apply_background_subtraction and not apply_background_division:
+            background_data[np.isnan(background_data)] = 0
+            background_data[background_data <= 1] = 0
+            det_data = det_data - background_data
 
     msk = (np.isfinite(det_data) & user_mask) * mask_h5
 
