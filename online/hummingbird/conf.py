3565854c3d27dbb78fdd73b28658b58ce3be0c2a
Ivan De Gennaro Aquino <degennaroaquino.ivan@outlook.com>
2023-11-26 08:09:42 +0100

    Update conf.py

--- online/hummingbird/conf.py
+++ online/hummingbird/conf.py
@@ -41,10 +41,11 @@ wavelength = 10.33e-10
 
 # streak finder parameters ---------------------------------------------
 
-streak_finder_thr_percent = 99.5
-streak_finder_min_pixels = 20
+streak_finder_thr_value = 50
+streak_finder_thr_is_percent = False
+streak_finder_min_pixels = 5
 streak_finder_max_area = 800
-streak_finder_num_streaks_threshold = 1
+streak_finder_num_streaks_threshold = 5
 
 # masks ---------------------------------------------------------------
 
@@ -60,17 +61,17 @@ mask_file = f"{prop_dir}/usr/Shared/hummingbird/mask_run128.h5"
 with h5py.File(mask_file, "r") as maskh5:
     mask_h5 = np.array(maskh5["entry_1/goodpixels"])
 
-ring_mask_file = f"{prop_dir}/usr/Shared/hummingbird/mask_run130.h5"
+ring_mask_file = f"{prop_dir}/usr/Shared/hummingbird/asic_gap_mask.h5"
 with h5py.File(ring_mask_file, "r") as maskh5:
-    ring_mask_h5 = np.array(maskh5["entry_1/good_pixels"])
+    ring_mask_h5 = np.array(maskh5["entry_1/goodpixels"])
 
-mask_h5 = ring_mask_h5 & mask_h5
+mask_h5_global = ring_mask_h5 & mask_h5 & user_mask
 
 # background whitefield file (dividing) ------------------------------------------
 
-whitefield_file = f"{prop_dir}/usr/Shared/hummingbird/white_field_run_128.h5"
+whitefield_file = f"{prop_dir}/usr/Shared/hummingbird/median_white_field_run_196.h5"
 with h5py.File(whitefield_file, "r") as bgh5:
-    background_data = np.array(bgh5["entry_1/data/white_field"])
+    bg_data = np.array(bgh5["entry_1/data/median_white_field"])
 
 # zeroing this for now
 # background_data = np.zeros(geom.expected_data_shape, float)
@@ -83,8 +84,8 @@ with h5py.File(whitefield_file, "r") as bgh5:
 
 # running white field background ------------------------------------------------
 
-running_wfs = [np.ones(geom.expected_data_shape, float)]
-length_running_white_fields = 1000
+running_white_fields = [np.ones(geom.expected_data_shape, float)]
+length_running_white_fields = 100
 
 # operations ------------------------------------------------------------------
 
@@ -93,21 +94,30 @@ do_streakfinding = True
 
 apply_running_background_division = False
 apply_background_division = False
-apply_background_subtraction = True
 
+apply_running_background_subtraction = True
+apply_background_subtraction = False
+
+# ---------------------------------------------------------
 
 if do_streakfinding:
     from streakfinder import StreakFinder
 
     streak_finder = StreakFinder(
-        thr_percent=streak_finder_thr_percent,
+        thr=streak_finder_thr_value,
         min_pixels=streak_finder_min_pixels,
         max_area=streak_finder_max_area,
+        thr_is_percent=streak_finder_thr_is_percent,
     )
     num_streaks_threshold = streak_finder_num_streaks_threshold
 
     streakogram = Peakogram(
-        geom, nr=500, rmin=0, rmax=1500, nintens=200, intens_max=200
+        geom,
+        nr=500,
+        rmin=0,
+        rmax=1500,
+        nintens=200,
+        intens_max=200,
     )
     streaks_powder = PseudoPowderDiffraction(geom)
 
@@ -139,7 +149,14 @@ if do_peakfinding:
     )
     num_peaks_threshold = 10
 
-    peakogram = Peakogram(geom, nr=500, rmin=0, rmax=1500, nintens=200, intens_max=200)
+    peakogram = Peakogram(
+        geom,
+        nr=500,
+        rmin=0,
+        rmax=1500,
+        nintens=200,
+        intens_max=200,
+    )
     peaks_powder = PseudoPowderDiffraction(geom)
 
 
@@ -152,7 +169,12 @@ if do_peakfinding:
 #         wavelength=wavelength,  # Wavelength (m)
 #     )
 
+
 def onEvent(evt):
+    global running_white_fields
+    background_data = bg_data.copy()
+    mask_h5 = mask_h5_global.copy()
+
     sys.stdout.flush()
     analysis.event.printProcessingRate()
 
@@ -160,15 +182,11 @@ def onEvent(evt):
     det_data = det.data[0]
 
     # roi plot (for pupil counts) ----------------------------------
-    
+
     roi_data = det_data[6, 20:340, 670:1000]
     intintens = np.nansum(roi_data)
-    intintens_rec = add_record(
-        evt["analysis"], "analysis", "roi_integral",  intintens
-    )
-    plotting.line.plotHistory(
-        intintens_rec, group="Hitfinding", history=10000
-    )
+    intintens_rec = add_record(evt["analysis"], "analysis", "roi_integral", intintens)
+    plotting.line.plotHistory(intintens_rec, group="Hitfinding", history=10000)
     image_roi = add_record(evt["analysis"], "analysis", "ROI", roi_data)
     plotting.image.plotImage(image_roi, group="Images", history=1)
 
@@ -176,28 +194,48 @@ def onEvent(evt):
         apply_background_division,
         apply_background_subtraction,
         apply_running_background_division,
+        apply_running_background_subtraction,
     ].count(True) > 1:
         raise Exception("wrong operation setup, one and only one should be true")
-
-    if apply_running_background_division:
-        if len(running_wfs) > length_running_white_fields:
-            running_wfs.pop(0)
-        running_wfs.append(det_data)
-
-        running_white_field = np.mean(running_wfs, axis=0, dtype=float)
-        running_white_field[~np.isfinite(running_white_field)] = 1
-        running_white_field[background_data <= 1] = 1
-        det_data = det_data / running_white_field
 
     if apply_background_division:
         background_data[~np.isfinite(background_data)] = 1
         background_data[background_data <= 1] = 1
         det_data = det_data / background_data
 
+    if apply_running_background_division:
+        if len(running_white_fields) > length_running_white_fields:
+            running_white_fields.pop(0)
+        running_white_fields.append(det_data)
+
+        running_white_field = np.mean(running_white_fields, axis=0, dtype=float)
+        running_white_field[~np.isfinite(running_white_field)] = 1
+        running_white_field[background_data <= 1] = 1
+        det_data = det_data / running_white_field
+
     if apply_background_subtraction:
         background_data[~np.isfinite(background_data)] = 0
         background_data[background_data <= 1] = 0
-        correction_factor = np.nansum(det_data * background_data) / np.nansum(background_data**2)
+        if np.nansum(background_data) < 1e-5:
+            correction_factor = 1.0
+        else:
+            correction_factor = np.nansum(det_data * background_data) / np.nansum(
+                background_data**2
+            )
+        det_data = det_data - correction_factor * background_data
+
+    if apply_running_background_subtraction:
+        if len(running_white_fields) > length_running_white_fields:
+            running_white_fields.pop(0)
+        running_white_fields.append(det_data)
+        running_white_field = np.mean(running_white_fields, axis=0, dtype=float)
+
+        running_white_field[~np.isfinite(running_white_field)] = 0
+        running_white_field[background_data <= 1] = 0
+        background_data = running_white_field.copy()
+        correction_factor = np.nansum(det_data * background_data) / np.nansum(
+            background_data**2
+        )
         det_data = det_data - correction_factor * background_data
 
     msk = (np.isfinite(det_data) & user_mask) * mask_h5
@@ -208,9 +246,29 @@ def onEvent(evt):
     assem, center = geom.position_modules_fast(masked_data)
     assem[np.isnan(assem)] = -1
 
-    #just_image = add_record(evt["analysis"], "analysis", "Image", assem)
-    #plotting.image.plotImage(just_image, group="Images", history=10, send_rate=2)
+    # just plot each image --------------------------------------------------------
 
+    just_image = add_record(evt["analysis"], "analysis", "Any image", assem)
+    plotting.image.plotImage(just_image, group="Images", history=10)
+
+    # sum total counts ----------------------------------------------------------
+
+    total_counts = np.nansum(masked_data)
+    total_counts_record = add_record(
+        evt["analysis"], "analysis", "total_counts", total_counts
+    )
+    plotting.line.plotHistory(
+        total_counts_record,
+        group="Total counts",
+        history=1000,
+        hline=float(np.nansum(background_data)),
+    )
+
+    if total_counts > float(np.nansum(background_data)):
+        image_hit_by_counts = add_record(
+            evt["analysis"], "analysis", "Hit (by counts)", assem
+        )
+        plotting.image.plotImage(image_hit_by_counts, group="Images", history=10)
 
     if do_peakfinding:
         peak_finder._mask = msk.astype(np.int8).reshape(-1, 512)
@@ -220,7 +278,7 @@ def onEvent(evt):
             evt["analysis"], "analysis", "numpeaks", peaks.num_peaks
         )
         plotting.line.plotHistory(
-            num_peaks_rec, group="Hitfinding", hline=num_peaks_threshold, history=10000
+            num_peaks_rec, group="Hitfinding", hline=num_peaks_threshold, history=1000
         )
 
         if peaks.num_peaks > 0:
@@ -229,7 +287,7 @@ def onEvent(evt):
                 evt["analysis"], "analysis", "Peaks powder", a
             )
             plotting.image.plotImage(
-                peaks_powder_image, group="Images", history=1, sum_over=True
+                peaks_powder_image, group="Stats", history=1, sum_over=True
             )
 
     if do_streakfinding:
@@ -239,7 +297,10 @@ def onEvent(evt):
             evt["analysis"], "analysis", "numstreaks", streaks.num_streaks
         )
         plotting.line.plotHistory(
-            num_streaks, group="Hitfinding", hline=num_streaks_threshold, history=10000
+            num_streaks,
+            group="Hitfinding",
+            hline=num_streaks_threshold,
+            history=1000,
         )
 
         if streaks.num_streaks > num_streaks_threshold:
@@ -249,7 +310,7 @@ def onEvent(evt):
                 evt["analysis"], "analysis", "Streaks powder (hits)", a
             )
             plotting.image.plotImage(
-                streaks_powder_hit, group="Images", history=1, sum_over=True
+                streaks_powder_hit, group="Stats", history=1, sum_over=True
             )
 
             b = streakogram.get(streaks.positions, streaks.intens[:, 0])
@@ -257,11 +318,11 @@ def onEvent(evt):
                 evt["analysis"], "analysis", "Streakogram", b
             )
             plotting.image.plotImage(
-                streakogram_image, group="Images", history=1, sum_over=True
+                streakogram_image, group="Stats", history=1, sum_over=True
             )
 
             image_hit = add_record(evt["analysis"], "analysis", "Hit", assem)
-            plotting.image.plotImage(image_hit, group="Images", history=10, send_rate=2)
+            plotting.image.plotImage(image_hit, group="Images", history=10)
 
             integral_hit = add_record(
                 evt["analysis"], "analysis", "Hit Integral", assem
@@ -278,7 +339,7 @@ def onEvent(evt):
                     evt["analysis"], "analysis", "Streaks powder (miss)", a
                 )
                 plotting.image.plotImage(
-                    streaks_powder_mis, group="Images", history=1, sum_over=True
+                    streaks_powder_mis, group="Stats", history=1, sum_over=True
                 )
 
             image_mis = add_record(evt["analysis"], "analysis", "Mis", assem)
@@ -292,7 +353,7 @@ def onEvent(evt):
             )
 
         # hitrate ---------------------------------------------
-        
+
         evt["analysis"]["isHit"] = streaks.num_streaks > num_streaks_threshold
         analysis.hitfinding.hitrate(
             evt,
@@ -301,5 +362,7 @@ def onEvent(evt):
         )
         if ipc.mpi.is_main_event_reader():
             plotting.line.plotHistory(
-                evt["analysis"]["hitrate"], group="Hitfinding", label="Hit rate [%]"
+                evt["analysis"]["hitrate"],
+                group="Hitfinding",
+                label="Hit rate [%]",
             )
